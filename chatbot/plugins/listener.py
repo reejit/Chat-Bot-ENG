752bc1d97fa4b869b37773e38b523aaf7407cf97
reejit <70316178+reejit@users.noreply.github.com>
2020-12-15 10:04:14 +0530

    Update listener.py

--- chatbot/plugins/listener.py
+++ chatbot/plugins/listener.py
@@ -24,7 +24,7 @@ async def chat_bot(client, message):
 async def check_message(client, message):
     if message.text.startswith == '/':
         return False
-    elif message.type == 'text':
+    elif pyrogram.message.type == 'text':
         return True
     Bot = await client.get_me()
     if message.text.lower() == f"@{Bot.username}":
